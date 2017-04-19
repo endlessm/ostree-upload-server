3f62e05c5832d006ba8b1f74ee62e750b882eb83
Lauri Myllari <lauri.myllari@movial.com>
2017-04-18 18:30:12 -0700

    tools: export-flatpak.sh to export a .flatpak from repository

---
+++ tools/export-flatpak.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# set safer error and list handling
+set -euo pipefail
+IFS=$'\n\t'
+
+repo=$1
+ref=$2
+
+tmp="${ref%/*/*/*}"
+IFS=/ read app arch ver <<< "${ref#"$tmp"/}"
+
+# echo app: $app
+# echo arch: $arch
+# echo ver: $ver
+
+ofname="${app}-${arch}-${ver}.flatpak"
+flatpak build-bundle "${repo}" "${ofname}" "${ref}"
