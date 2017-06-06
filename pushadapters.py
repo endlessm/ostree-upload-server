6084c789436c43e36973bbb3b3347f792eb072b3
Lauri Myllari <lauri.myllari@movial.com>
2017-06-05 18:49:28 -0700

    start adding adapters for pushing apps

---
+++ pushadapters.py
@@ -0,0 +1,23 @@
+
+
+adapters = {
+               'http': HTTPPushAdapter,
+               'scp': SCPPushAdapter
+           }
+
+class BasePushAdapter():
+    def __init__(self):
+        pass
+
+
+class HTTPPushAdapter(BasePushAdapter):
+    def __init__(self):
+        pass
+
+
+class SCPPushAdapter(BasePushAdapter):
+    def __init__(self):
+        pass
+
+
+
