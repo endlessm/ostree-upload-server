e27598c1ddce646a8b00d3f4812f4e07287ca55d
Lauri Myllari <lauri.myllari@movial.com>
2017-06-07 15:39:35 -0700

    Split Task from ostree-upload-server

--- ostree-upload-server.py
+++ ostree-upload-server.py
@@ -19,42 +19,9 @@ from gevent.subprocess import check_output, CalledProcessError, STDOUT
 from flask import Flask, jsonify, request, render_template, send_from_directory, url_for
 
 from pushadapters import adapters
+from task import Task, TaskState
 
 MAINTENANCE_WAIT = 10
-
-
-class TaskState:
-    Pending, Processing, Completed, Failed = range(4)
-
-
-class Task:
-    _next_task_id = 0
-
-    def __init__(self, name, data):
-        self._task_id = Task._next_task_id
-        Task._next_task_id += 1
-        self._name = name
-        self._data = data
-        self._state = TaskState.Pending
-        self._state_change = Event()
-
-    def set_state(self, newstate):
-        self._state = newstate
-        self._state_change.set()
-        gsleep(0) # wake up anyone waiting
-        self._state_change.clear()
-
-    def get_name(self):
-        return self._name
-
-    def get_data(self):
-        return self._data
-
-    def get_state(self):
-        return self._state
-
-    def get_id(self):
-        return self._task_id
 
 
 class TaskList:
