128b5050db24a33bdc9fd81a48549c00ea8ce07a
Lauri Myllari <lauri.myllari@movial.com>
2017-06-07 21:44:36 -0700

    Try to extract flatpak for pushing

--- task.py
+++ task.py
@@ -63,11 +63,41 @@ class ReceiveTask(BaseTask):
 
 
 class PushTask(BaseTask):
-    def __init__(self, taskname, ref, remote):
+    def __init__(self, taskname, repo, ref, adapter, tempdir):
         super(ReceiveTask, self).__init__(taskname)
+        self._repo = repo
         self._ref = ref
-        self._remote = remote
+        self._adapter = adapter
+        self._tempdir = tempdir
 
     def run(self):
         logging.info("processing task " + self.get_name())
-        logging.debug("push {0} to {1} ".format(self._ref, self._remote))
+        logging.debug("push {0} to {1} ".format(self._ref, self._adapter))
+        bundle = self._extract()
+        if not bundle:
+            logging.error("failed to extract {0}".format(self._ref))
+            self.set_state(TaskState.Failed)
+            return
+        if not self._adapter.push(bundle):
+            logging.error("failed to push {0} to {1}".format(bundle, self._adapter))
+            self.set_state(TaskState.Failed)
+            return
+        os.unlink(bundle)
+        self.set_state(TaskState.Completed)
+        logging.info("completed task " + self.get_name())
+
+    def _extract(self):
+        (f, filename) = tempfile.mkstemp(dir=self._tempdir)
+        os.close(f)
+        try:
+            output = check_output(["flatpak",
+                                   "build-bundle",
+                                   self._repo,
+                                   filename,
+                                   self._ref],
+                                  stderr=STDOUT)
+            logging.info("extracted {0} as {1}".format(self._ref, filename))
+            return filename
+        except CalledProcessError as e:
+            os.unlink(filename)
+            return None
