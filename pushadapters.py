7083f8847d859d5f042cac05e0dfd6a19af77d37
Lauri Myllari <lauri.myllari@movial.com>
2017-06-14 18:12:02 -0700

    Authentication support for HTTP push

--- pushadapters.py
+++ pushadapters.py
@@ -28,6 +28,12 @@ class HTTPPushAdapter(BasePushAdapter):
     def __init__(self, name, settings):
         super(HTTPPushAdapter, self).__init__(name)
         self._url = settings.get('url')
+        username = settings.get('username')
+        password = settings.get('password')
+        if username and password:
+            self._auth = requests.auth.HTTPBasicAuth(username, password)
+        else:
+            self._auth = None
 
     def push(self, bundle):
         logging.debug("http push {0} to {1}".format(bundle, self._url))
@@ -36,6 +42,7 @@ class HTTPPushAdapter(BasePushAdapter):
                                              'application/octet-stream')})
         r = requests.post(self._url,
                           data=encoder,
+                          auth=self._auth,
                           headers={'Content-Type': encoder.content_type})
         logging.debug("http push {0} response: {1}".format(bundle, r.text))
         return r.status_code == requests.codes.ok
