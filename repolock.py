8e92a1b57c33b48d8ef8815f72f44e4ab5171855
Lauri Myllari <lauri.myllari@movial.com>
2017-06-22 17:32:46 -0700

    Clean up unnecessary imports from repolock

--- repolock.py
+++ repolock.py
@@ -16,11 +16,9 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import contextlib
 import errno
 import fcntl
 from itertools import imap
-from io import open
 import logging
 import os
 import time
